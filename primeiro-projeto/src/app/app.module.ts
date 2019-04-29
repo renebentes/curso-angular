5d907f10e21e50d13dad50e9b538de4dab2e94da
Rene Bentes Pinto <renebpinto@outlook.com>
2019-04-29 14:14:47 -0300

    Cria o primeiro componente manualmente

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MeuPrimeiroComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
